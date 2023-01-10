204ca83290ae96f2af7496077260cd21819299a6
Juan Millan <jmillan@catalyte.io>
2023-01-09 20:51:38 -0600

    header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: MainPageComponent },
+  // { path: '/patients', component: PatientsComponent },
+  // { path: '/patients/:id', component: PatientDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
